4682edf7dd6ed4f9129bb4a38cee4b2b105d3833
yurigligoric <yurigligoric@gmail.com>
2024-01-19 19:28:22 +0800

    object

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+var Access;
+(function (Access) {
+    Access[Access["Admin"] = 0] = "Admin";
+    Access[Access["Moderator"] = 1] = "Moderator";
+    Access[Access["User"] = 2] = "User";
+})(Access || (Access = {}));
 var student = {
     name: 'Juan',
     age: 129,
-    interest: ["basketball", "pingpong"]
+    interest: ["basketball", "pingpong"],
+    access: Access.Admin
 };
-console.log(student.interest);
+console.log(student.access);
